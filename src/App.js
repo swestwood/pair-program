90104c75e0da4f63c3861b9a1d1676372f36ba57
Sophia <sophiawestwood@gmail.com>
2018-08-14 22:52:10 -0700

    Initial commit
    Cloned from https://github.com/aws-samples/aws-mobile-appsync-events-starter-react

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import "semantic-ui-css/semantic.min.css";
+import 'react-datepicker/dist/react-datepicker.css';
+
+import appSyncConfig from "./AppSync";
+import { ApolloProvider } from "react-apollo";
+import AWSAppSyncClient, { defaultDataIdFromObject } from "aws-appsync";
+import { Rehydrated } from "aws-appsync-react";
+
+import './App.css';
+import AllEvents from './Components/AllEvents';
+import NewEvent from './Components/NewEvent';
+import ViewEvent from './Components/ViewEvent';
+
+const Home = () => (
+  <div className="ui container">
+    <AllEvents />
+  </div>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/event/:id" component={ViewEvent} />
+      <Route path="/newEvent" component={NewEvent} />
+    </div>
+  </Router>
+);
+
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.graphqlEndpoint,
+  region: appSyncConfig.region,
+  auth: {
+    type: appSyncConfig.authenticationType,
+    apiKey: appSyncConfig.apiKey,
+  },
+  cacheOptions: {
+    dataIdFromObject: (obj) => {
+      let id = defaultDataIdFromObject(obj);
+
+      if (!id) {
+        const { __typename: typename } = obj;
+        switch (typename) {
+          case 'Comment':
+            return `${typename}:${obj.commentId}`;
+          default:
+            return id;
+        }
+      }
+
+      return id;
+    }
+  }
+});
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
+
+export default WithProvider;
